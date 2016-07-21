a3d1514cab86eeeceeac0f03d89bc2d809bcd078
gitgeekaus <adrianwang.aus@gmail.com>
2016-07-22 02:04:26 +1000

    test sample

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -2,10 +2,121 @@
 
 namespace App\Http\Controllers;
 
-class PostsController extends Controller
-{
-    public function index()
-    {
-        return view('posts.index');
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class PostsController extends Controller {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index() {
+        $posts = \App\Post::orderBy('created_at', 'desc')->get();
+        return view('posts.index', compact('posts'));
     }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create() {
+        $posts = \App\Post::orderBy('created_at', 'desc')->get();
+        return view('posts.create', compact('posts'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request) {
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            if ($user) {
+                $input = $request->all();
+                $post = new \App\Post();
+                $post->user_id = $user->id;
+                $post->title = $input['title'];
+                $post->content = $input['content'];
+                $post->save();
+                return redirect('posts');
+            }
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id) {
+        if (\Auth::check()) {
+            $post = \App\Post::find($id);
+            return view('posts.show', compact('post'));
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id) {
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            if ($user) {
+                $post = $user->post()->find($id);
+                return view('posts.edit', compact('post'));
+            }
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id) {
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            if ($user) {
+                $post = $user->post()->find($id);
+                if ($post) {
+                    $input = $request->all();
+                    $post->title = $input['title'];
+                    $post->content = $input['content'];
+                    $post->save();
+                }
+                return redirect('posts');
+            }
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id) {
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            if ($user) {
+                $post = $user->post()->find($id);
+                if ($post) {
+                    $post->delete();
+                }
+               return redirect('posts');
+            }
+        }
+    }
+
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,7 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
 Route::auth();
 
-Route::get('/', 'PostsController@index');
+Route::resource('posts', 'PostsController');
+Route::get('/','PostsController@index');
